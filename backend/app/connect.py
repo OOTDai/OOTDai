6814511882cf5c3dc3d674f496939ef271bcf5c3
Andy Guajardo <andymatt1225@gmail.com>
2025-03-24 21:06:10 -0700

    connect.py file added, more docs added to file structure

---
+++ backend/app/connect.py
@@ -0,0 +1,9 @@
+# This file contains the connection to the database inlcuding configurations, connection logic, and any functions needed to connect to the database
+
+
+# will most likely using SQLAlchemy to connect to the database
+# SQLAlchemy is a SQL toolkit and Object-Relational Mapping (ORM) library for Python
+# It provides a full suite of well-known enterprise-level persistence patterns, designed for efficient and high-performing database access
+
+
+
