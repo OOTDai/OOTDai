8bbc83c8abe806ed2405d382820f8c582dcf5b8c
Daniel <99973059+heymistersin@users.noreply.github.com>
2025-04-08 23:13:04 -0400

    Added markdown for psql instructions

--- connect.py
+++ connect.py
@@ -9,18 +9,24 @@ def connect_to_rds():
     try:
         connection = psycopg2.connect(
             host=os.getenv("DB_HOST"),
-            database=os.getenv("DB_NAME"),
+            database=os.getenv("DB_NAME"), #RDS > Databases > Configuration > DB name (if blank, try 'postgres')
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PW"),
             port=os.getenv("DB_PORT"),
         )
 
         print("RDS connection successful!")
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT current_user;")
+        print("Current user:", cursor.fetchone()[0])
+
         connection.close()
 
     except OperationalError as e:
         print("Failed to connect to RDS!")
         print(e)
+
 
 if __name__ == "__main__":
     connect_to_rds()
